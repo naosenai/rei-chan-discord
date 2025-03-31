32d1cc0370657c5366a39d2e6b59ec7c6e22147e
naosenai <naosenai@proton.me>
2025-03-31 15:39:36 -0500

    Rewrote embed code

--- cogs/music/subcogs/lyrics/lyrics.py
+++ cogs/music/subcogs/lyrics/lyrics.py
@@ -1,12 +1,11 @@
 import aiohttp
 import io
-from enum import Enum
 from PIL import Image
 
 import discord
 from discord import app_commands
 from discord.ext import commands
-from discord.ui import Button, View
+from discord.ui import View
 
 from .components import vocaloid_scraper as vs
     
@@ -19,11 +18,11 @@ class LyricsSession:
         self.user = interaction.user
         self.msg = None
 
-        self.lyrics_data = None
-        self.lyrics_color = None
-        self.lyrics_extras = None
-        self.lyrics_video = None
-        self.lyrics_video_msg = None
+        self.data = None
+        self.color = None
+        self.external_links = None
+        self.video = None
+        self.video_msg = None
         self.lyrics_page = None
 
         #This will be revised for localization later
@@ -35,10 +34,10 @@ class LyricsSession:
             color=discord.Color.orange())
         
         self.msg = await self.interaction.followup.send(embed=embed)
-        self.lyrics_data = vs.Song(self.query)
-        self.lyrics_color = await self.get_average_color(self.lyrics_data.image)
-        self.lyrics_extras = "\n".join([f"• [{link['title']}]({link['href']})" for link in self.lyrics_data.links])
-        self.lyrics_video = next((link['href'] for link in self.lyrics_data.links if link['title'] == "YouTube Broadcast"), None)
+        self.data = vs.Song(self.query)
+        self.color = await self.get_average_color(self.data.image)
+        self.external_links = "\n".join([f"• [{link['title']}]({link['href']})" for link in self.data.links])
+        self.video = next((link['href'] for link in self.data.links if link['title'] == "YouTube Broadcast"), None)
         self.lyrics_page = ["Original", 0]
 
     async def get_average_color(self, image_url: str) -> int:
@@ -56,139 +55,104 @@ class LyricsSession:
         avg_color_hex = (avg_color[0] << 16) + (avg_color[1] << 8) + avg_color[2]
         return avg_color_hex
 
-class ButtonType(Enum):
-    PAGE = "page"
-    DELETE = "delete"
-    SELECTOR = "selector"
-    LYRICS = "lyrics"
-    AI_TRANSLATE = "ai_translate" # Not implemented
-    YOUTUBE = "youtube"
-
-class BaseButton(Button):
-    def __init__(self, label, row, button_type: ButtonType, session: LyricsSession, callback_func=None, **kwargs):
-        self.button_type = button_type
-        self.callback_func = callback_func
+class LyricsView(View):
+    def __init__(self, session: LyricsSession):
+        super().__init__(timeout=600)
         self.session = session
-        super().__init__(label=label, row=row, **kwargs)
-        
-    async def callback(self, interaction: discord.Interaction):
+        self.disable_buttons()
+
+    def disable_buttons(self):
+        disabled_list = [
+            self.session.lyrics_page[1] == 0,
+            self.session.lyrics_page[1] == 1 or len(self.session.data.lyrics) < 2 or self.session.data.lyrics[1] == "",
+            self.session.lyrics_page[1] == 2 or len(self.session.data.lyrics) < 3 or self.session.data.lyrics[2] == "",
+            not self.session.video
+            ]
+        for item in self.children:
+            if isinstance(item, discord.ui.Button):
+                if item.custom_id in {"0", "1", "2", "3"}:
+                    item.disabled = disabled_list[int(item.custom_id)]
+
+    def stop(self):
+        super().stop()
+        self.clear_items()
+
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
         if interaction.user != self.session.user:
             await interaction.response.send_message("Only the command sender can use these buttons!", ephemeral=True)
+            return False
+        if not interaction.response.is_done():
+            await interaction.response.defer()
+        return True
+    
+    async def lyrics_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if not await self.interaction_check(interaction):
             return
-        await interaction.response.defer()
+        self.stop()
+        self.session.lyrics_page = [button.label, int(button.custom_id)]
+        await lyrics_embed(self.session)
+        await lyrics_view(self.session)
 
-        if self.callback_func:
-            await self.callback_func(interaction, self)
+    @discord.ui.button(label="Original", row=1, custom_id='0', style=discord.ButtonStyle.primary)
+    async def original_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await self.lyrics_button(interaction, button)
 
-async def delete_callback(_interaction, button):
-    session = button.session
-    if session.lyrics_video_msg:
-        await session.lyrics_video_msg.delete()
-        session.lyrics_video_msg = None
-    await session.msg.delete()
+    @discord.ui.button(label="Romanized", row=1, custom_id='1', style=discord.ButtonStyle.primary)
+    async def romanized_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await self.lyrics_button(interaction, button)
 
-async def lyrics_callback(_interaction, button):
-    button.view.stop()
-    button.view.clear_items()
-    session = button.session
-    session.lyrics_page = [button.label, int(button.custom_id)]
-    await lyrics_embed(session)
-    await lyrics_view(session)
+    @discord.ui.button(label="Translated", row=1, custom_id='2', style=discord.ButtonStyle.primary)
+    async def translated_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await self.lyrics_button(interaction, button)
 
-async def youtube_callback(interaction, button):
-    session = button.session
-    if not session.lyrics_video:
-        await interaction.response.send_message("No valid video for this song.", ephemeral=True)
-        return
-    if not session.lyrics_video_msg:
-        session.lyrics_video_msg = await interaction.followup.send(content=f"{session.lyrics_video}")
-    else:
-        await session.lyrics_video_msg.delete()
-        session.lyrics_video_msg = None
-    await lyrics_embed(session)
+    @discord.ui.button(label="YouTube Popout", row=2, custom_id='3', style=discord.ButtonStyle.secondary)
+    async def youtube_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if not await self.interaction_check(interaction):
+            return
+        if not self.session.video_msg:
+            self.session.video_msg = await interaction.followup.send(content=f"{self.session.video}")
+        else:
+            await self.session.video_msg.delete()
+            self.session.video_msg = None
+        await lyrics_embed(self.session)
 
-
-
-
-async def nothing_found(session) -> None:
-    embed = discord.Embed(
-        title=f"No results found. If you think this is a mistake, it probably is and im too lazy to fix it.",
-        color=discord.Color.orange())
-    await session.msg.edit(embed=embed)
+    @discord.ui.button(label="✖", row=2, style=discord.ButtonStyle.danger)
+    async def delete_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if not await self.interaction_check(interaction):
+            return
+        if self.session.video_msg:
+            await self.session.video_msg.delete()
+        await self.session.msg.delete()
 
 async def lyrics_embed(session):
     embed = discord.Embed(
-        url=session.lyrics_data.query,
-        title=f"{session.lyrics_page[0]} lyrics for {session.lyrics_data.title}",
-        color=discord.Color(session.lyrics_color),
-        description=session.lyrics_data.lyrics[session.lyrics_page[1]],
+        url=session.data.query,
+        title=f"{session.lyrics_page[0]} lyrics for {session.data.title}",
+        color=discord.Color(session.color),
+        description=session.data.lyrics[session.lyrics_page[1]],
     )
 
     embed.add_field(name=f"\u200B", value="\u200B") # Padding between lyrics & extras
-    embed.add_field(name=f"═ External Links ═", value=session.lyrics_extras, inline=False)
+    embed.add_field(name=f"═ External Links ═", value=session.external_links, inline=False)
 
-    if not session.lyrics_video_msg:
-        embed.set_image(url=session.lyrics_data.image)
+    if not session.video_msg:
+        embed.set_image(url=session.data.image)
 
     embed.set_footer(text=session.embed_footer, icon_url=session.user.avatar.url)
 
     await session.msg.edit(embed=embed)
 
-
 async def lyrics_view(session):
-    view = View(timeout=600)
-    original_button = BaseButton(label="Original",
-                                 row=1,
-                                 button_type=ButtonType.LYRICS,
-                                 session=session,
-                                 callback_func=lyrics_callback,
-                                 style=discord.ButtonStyle.primary,
-                                 custom_id="0",
-                                 disabled=session.lyrics_page[1] == 0
-    )
-    romanized_button = BaseButton(label="Romanized",    
-                                  row=1,
-                                  button_type=ButtonType.LYRICS,
-                                  session=session,
-                                  callback_func=lyrics_callback,
-                                  style=discord.ButtonStyle.primary,
-                                  custom_id="1",
-                                  disabled=session.lyrics_page[1] == 1 or len(session.lyrics_data.lyrics) < 2 or session.lyrics_data.lyrics[1] == ""
-    )
-    translated_button = BaseButton(label="Translated", 
-                                   row=1,
-                                   button_type=ButtonType.LYRICS,
-                                   session=session,
-                                   callback_func=lyrics_callback,
-                                   style=discord.ButtonStyle.primary,
-                                   custom_id="2",
-                                   disabled=session.lyrics_page[1] == 2 or len(session.lyrics_data.lyrics) < 3 or session.lyrics_data.lyrics[2] == ""
-    )
-
-    youtube_button = BaseButton(label="YouTube Popout", 
-                               row=2,
-                               button_type=ButtonType.YOUTUBE,
-                               session=session,
-                               callback_func=youtube_callback,
-                               disabled=not session.lyrics_video
-    )
-    delete_button = BaseButton(label="✖", 
-                             row=2,
-                             button_type=ButtonType.DELETE,
-                             session=session,
-                             callback_func=delete_callback,
-                             style=discord.ButtonStyle.danger
-    )
-    view.add_item(original_button)
-    view.add_item(romanized_button)
-    view.add_item(translated_button)
-    view.add_item(youtube_button)
-    view.add_item(delete_button)
+    view = LyricsView(session)
     await session.msg.edit(view=view)
 
 async def initialize_lyrics(session):
-    if not session.lyrics_data.lyrics:
-        await nothing_found(session)
+    if not session.data.lyrics:
+        embed = discord.Embed(
+            title=f"No lyrics discovered for {session.data.title}",
+            color=discord.Color.orange()
+        )
+        await session.msg.edit(embed=embed)
         return
     await lyrics_embed(session)
     await lyrics_view(session)
@@ -219,7 +183,6 @@ class Lyrics(commands.Cog):
     @app_commands.allowed_installs(guilds=True, users=True)
     @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
     @app_commands.command(name="lyrics", description="Enter a name of a vocaloid song to search Fandom for its lyrics!")
-    #@app_commands.autocomplete(search=lyrics_autocomplete)
     async def lyrics(self, interaction: discord.Interaction, search: str):
         try:
             search = await self.lyrics_fallback(interaction, search)
