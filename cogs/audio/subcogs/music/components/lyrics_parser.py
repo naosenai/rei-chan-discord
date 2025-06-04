73351f16da1e73feb01232942bd2a05eb79787cf
naosenai <naosenai@proton.me>
2025-06-04 16:48:58 -0500

    Update lyrics parser

--- cogs/audio/subcogs/music/components/lyrics_parser.py
+++ cogs/audio/subcogs/music/components/lyrics_parser.py
@@ -1,6 +1,13 @@
 import requests
+
 import mwparserfromhell as mw
-from typing import Callable
+from mwparserfromhell.nodes.template import Parameter, Template
+from mwparserfromhell.nodes.wikilink import Wikilink
+from mwparserfromhell.nodes.external_link import ExternalLink
+from mwparserfromhell.nodes.text import Text
+from mwparserfromhell.smart_list import SmartList
+
+from typing import Callable, Any
 from functools import singledispatchmethod
 
 
@@ -8,10 +15,14 @@ from functools import singledispatchmethod
 def inject_field(*fields: str) -> Callable:
     def decorator(func):
         def wrapper(self, *args, **kwargs):
-            if not self.template: self.set_template()
+            if not self.wiki_code: return None
+            if not self.template: 
+                if not self.set_template():
+                    return None
             for field in fields:
                 if self.template.has(field):
-                    return func(self, self.template.get(field).value, *args, **kwargs)
+                    self._field = self.template.get(field).value
+                    return func(self, *args, **kwargs)
         return wrapper
     return decorator
 
@@ -23,7 +34,8 @@ def inject_section(main_heading: str, subheading: str|None = None) -> Callable:
             if subheading:
                 section = section[0].get_sections(matches=subheading, include_lead=False, include_headings=False)
                 if not section: return None
-            return func(self, section[0], *args, **kwargs)
+            self._section = section[0]
+            return func(self, *args, **kwargs)
         return wrapper
     return decorator
 
@@ -33,23 +45,24 @@ class ParserBase:
     def __init__(self):
         self.wiki_url = "https://vocaloidlyrics.fandom.com"
         self.headers = {"User-Agent": "Rei-Chan/1.0"}
-        self.query = None
-        self.page = None
-        self.wiki_code = None
-        self.template = None
         self.TYPE_LABELS = {
-            mw.nodes.template.Template: "template",
-            mw.nodes.wikilink.Wikilink: "wikilink",
-            mw.nodes.external_link.ExternalLink: "external"
+            Template: "template",
+            Wikilink: "wikilink",
+            ExternalLink: "external"
         }
+
+        self.query: str|None = None
+        self.page: str|None = None
+        self.wiki_code: mw.wikicode.Wikicode|None = None
+        self.template: mw.wikicode.Template|None = None
     
-    def request_query(self) -> list[any]|None:
+    def request_query(self) -> list[dict[str,str]]|None:
         url = f"{self.wiki_url}/api.php?action=query&list=search&srsearch={self.query}&format=json"
         res = requests.get(url, headers=self.headers)
         if res.status_code == 200:
             try:
                 results = res.json()['query']['search']
-                return [(r['title'], f"{self.wiki_url}/wiki/{r['title'].replace(' ', '_')}") for r in results]
+                return [{"label": r['title'], "link": f"{self.wiki_url}/wiki/{r['title'].replace(' ', '_')}"} for r in results]
             except KeyError:
                 return
 
@@ -63,10 +76,19 @@ class ParserBase:
                 return True
         return False
     
-    def clean(self, node, targ: str = None):
+    def set_template(self, alternate: bool = False) -> bool|None:
+        if not self.wiki_code: return None
+        template_name = "infobox_song" if not alternate else "alternateversion"
+        for template in self.wiki_code.filter_templates():
+            if template_name in self.clean(template.name).lower():
+                self.template = template
+                return True
+        return False
+    
+    def clean(self, node, targ: str|None = None):
         return node.strip_code().strip(targ)
     
-    def parse_title(self, line) -> dict:
+    def parse_title(self, line) -> dict|None:
         text = self.clean(mw.parse(line), '\'" ')
         if not text: return None
         if ':' in text: 
@@ -76,26 +98,26 @@ class ParserBase:
             return {"language": None, "title": text}
         
     @singledispatchmethod
-    def parse_node(self, node):
+    def parse_node(self, node: Any) -> dict|None:
         return None
     
-    @parse_node.register(mw.nodes.template.Template)
-    def template_link(self, node) -> dict|None:
+    @parse_node.register(Template)
+    def template_link(self, node: Template) -> dict|None:
         if node.name.strip().lower() == "vdb" and node.has(1):
             return {"label": "VocaDB", "link": f"https://vocadb.net/{self.clean(node.get(1).value)}"}
         elif node.has(1):
             title = self.clean(node.get(1).value)
             return {"label": title, "link": f"{self.wiki_url}/wiki/{title.replace(' ', '_')}"}
         
-    @parse_node.register(mw.nodes.wikilink.Wikilink)
-    def wiki_link(self, node) -> dict|None:
+    @parse_node.register(Wikilink)
+    def wiki_link(self, node: Wikilink) -> dict|None:
         label = title = self.clean(node.title or node.text)
         if title.lower().startswith("category:"): 
             label = title.split(":", 1)[1].strip()
         return {"label": label, "link": f"{self.wiki_url}/wiki/{title.replace(' ', '_')}"}
     
-    @parse_node.register(mw.nodes.external_link.ExternalLink)
-    def external_link(self, node) -> dict|None:
+    @parse_node.register(ExternalLink)
+    def external_link(self, node: ExternalLink) -> dict|None:
         return {"label": self.clean(node.title), "link": self.clean(node.url)}
     
     def parse_links(self, source, *, include=("external", "wikilink", "template"), filter_func=None, enrich=None) -> list[dict]|None:
@@ -113,7 +135,7 @@ class ParserBase:
     
     # Enrich function for parse_links
     def link_notes(self, node, link, links) -> None:
-        if isinstance(node, mw.nodes.text.Text):
+        if isinstance(node, Text):
             text = node.strip()
             if text.startswith("(") and text.endswith(")") and links:
                 links[-1]["note"] = text.strip("( )")
@@ -123,89 +145,113 @@ class ParserBase:
 class VocaloidParser(ParserBase):
     def __init__(self):
         super().__init__()
+        self._field: Parameter
+        self._section: SmartList
 
-    def get_query_results(self, query):
+    def get_query_results(self, query: str):
         self.query = query
         return self.request_query()
     
-    def set_page(self, page):
-        self.page = page
-        return self.request_page()
+    def set_page(self, page: str|None = None, alternate: bool = False):
+        if not self.page and not page: return
+        if not self.page and page: self.page = page
+        return self.request_page() and self.set_template(alternate)
     
-    def set_template(self, do_alternate: bool = False) -> bool:
-        template_name = "infobox_song" if not do_alternate else "alternateversion"
-        for template in self.wiki_code.filter_templates():
-            if template_name in self.clean(template.name).lower():
-                self.template = template
-                return True
-        return False
-    
-    # TODO: Add a boolean for epilepsy.
     def get_content_warning(self):
-        return next((
-            {"type": name.capitalize() ,
-            "label": self.clean(template.get(1).value)}
-            for template in self.wiki_code.filter_templates()
-            if (name := template.name.lower().strip()) in {"explicit", "questionable"}
-        ), None)
+        if not self.wiki_code: return None
+
+        epilepsy_present = False
+        for template in self.wiki_code.filter_templates():
+            name = template.name.lower().strip()
+            if name == "epilepsy": epilepsy_present = True
+            if name in {"explicit", "questionable"} and template.has(1):
+                return {
+                    "type": name.capitalize(),
+                    "label": self.clean(template.get(1).value),
+                    "epilepsy": epilepsy_present
+                }
+
+        return {"type": None, "label": None, "epilepsy": True} if epilepsy_present else None
     
     @inject_field('image')
-    def get_image(self, field):
-        return f"{self.wiki_url}/wiki/File:{self.clean(field).replace(' ', '_')}"
+    def get_image(self):
+        return f"{self.wiki_url}/wiki/File:{self.clean(self._field).replace(' ', '_')}"
     
     @inject_field('songtitle', 'title')
-    def get_titles(self, field):
-        lines = str(field).replace("<br />", "\n").replace("<br>", "\n").split("\n")
+    def get_titles(self):
+        lines = str(self._field).replace("<br />", "\n").replace("<br>", "\n").split("\n")
         return [title for line in filter(None, lines) if (title := self.parse_title(line))] or None
     
     @inject_field('color')
-    def get_color(self, field):
-        colors = tuple(map(str.strip, field.split("; color:", 1)))
+    def get_color(self):
+        colors = tuple(map(str.strip, self._field.split("; color:", 1)))
         return {"main": colors[0], "highlight": colors[1] if len(colors) > 1 else None}
 
     @inject_field('original upload date', 'date')
-    def get_date(self, field):
-        date_template = field.filter_templates()
-        if not date_template: return self.clean(field)
+    def get_date(self):
+        date_template = self._field.filter_templates()
+        if not date_template: return self.clean(self._field)
         get = lambda n: self.clean(date_template[0].get(n).value)
         return f"{get(1)} {get(2)}, {get(3)}"
 
     @inject_field('singer')
-    def get_singers(self, field):
-        return self.parse_links(field, include=["wikilink", "template"])
+    def get_singers(self):
+        return self.parse_links(self._field, include=["wikilink", "template"])
 
     @inject_field('producer')
-    def get_producers(self, field):
-        return self.parse_links(field, enrich=self.link_notes)
+    def get_producers(self):
+        return self.parse_links(self._field, enrich=self.link_notes)
 
     @inject_field('#views')
-    def get_views(self, field):
-        return self.clean(field) or None
+    def get_views(self):
+        return self.clean(self._field) or None
 
     @inject_field('link')
-    def get_links(self, field):
-        return self.parse_links(field, include=["external"])
+    def get_links(self):
+        return self.parse_links(self._field, include=["external"])
 
     @inject_field('description')
-    def get_description(self, field):
-        return self.clean(field) or None
+    def get_description(self):
+        return self.clean(self._field) or None
     
     # TODO: Parse lyrics.
+    @inject_section('Lyrics')
     def get_lyrics(self):
-        return
+        print("[DEBUG] Parsing lyrics headers")
+
+        lines = str(self._section).splitlines()
+
+        headers = None
+        for line in lines:
+            line = line.strip()
+            if line.startswith("|") and "||" in line:
+                raw_cells = line.strip("|").split("||")
+                headers = [self.clean(mw.parse(cell), "'") for cell in raw_cells]
+                break 
+
+        if not headers:
+            print("[DEBUG] No header row found.")
+            return None
+
+        print(f"[DEBUG] Found headers: {headers}")
+
+        lyrics_data = {header: [] for header in headers}
+        print(f"[DEBUG] Initialized lyrics data structure: {lyrics_data}")
+
+        return lyrics_data
             
     @inject_section('External Links')
-    def get_external_links(self, section):
-        return self.parse_links(section, include=["external"])
+    def get_external_links(self):
+        return self.parse_links(self._section, include=["external"])
     
     @inject_section('External Links', 'Unofficial')
-    def get_unofficial_links(self, section):
-        return self.parse_links(section, include=["external", "template"])
+    def get_unofficial_links(self):
+        return self.parse_links(self._section, include=["external", "template"])
 
     @inject_section('External Links', 'Unofficial')
-    def get_categories(self, section):
-        filter = lambda node: isinstance(node, mw.nodes.wikilink.Wikilink) and self.clean(node.title).startswith("Category:")
-        return self.parse_links(section, include=["wikilink"], filter_func=filter)
+    def get_categories(self):
+        filter = lambda node: isinstance(node, Wikilink) and self.clean(node.title).startswith("Category:")
+        return self.parse_links(self._section, include=["wikilink"], filter_func=filter)
     
     def extract_page(self):
         return {
@@ -228,18 +274,18 @@ class VocaloidParser(ParserBase):
 
 
 page_query = "BUTCHER_VANITY"
-page_query = "Miss Death's Idol"
-page_query = "のろい_(Noroi)"
-page_query = "カゼマチグサ_(Kazemachigusa)"
+#page_query = "Miss Death's Idol"
+#page_query = "のろい_(Noroi)"
+#page_query = "カゼマチグサ_(Kazemachigusa)"
+#page_query = "Splitter_Girl"
 
 song_info = VocaloidParser()
-print(song_info.get_query_results(page_query))
-song_info.set_page(page_query)
+#print(song_info.get_query_results(page_query))
 
-print("\ntemplate:")
-print(song_info.set_template(do_alternate=False))
-print(song_info.template)
+song_info.set_page(page_query, alternate=False)
 
+song_info.get_lyrics()
+'''
 stuff = song_info.extract_page()
 for x in stuff:
-    print(f"\n{x}:\n{stuff[x]}")
+    print(f"\n{x}:\n{stuff[x]}")'''
